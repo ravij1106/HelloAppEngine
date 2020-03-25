0414add91c1ac817d5ae12553419d27a8eb4b5f0
ravij1106 <59053064+ravij1106@users.noreply.github.com>
2020-03-25 10:59:13 +0530

    Add files via upload
    Added controller in project

---
+++ src/test/java/com/appengine/HelloAppEngine/HelloAppEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appengine.HelloAppEngine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloAppEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
